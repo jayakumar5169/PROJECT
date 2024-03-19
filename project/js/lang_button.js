d1e2763ad7accf99424ac88840bf7cfe7e57c367
jayakumar5169 <163168098+jayakumar5169@users.noreply.github.com>
2024-03-19 18:38:31 +0530

    Add files via upload

---
+++ project/js/lang_button.js
@@ -0,0 +1,25 @@
+function open_window_eng()
+        {
+            window.open("http://localhost/project/lang/english.html");
+            window.close("new_search.html");
+        }
+        function open_window_tel()
+        {
+            window.open("http://localhost/project/lang/tel.html");
+            window.close("new_search.html");
+        }
+        function open_window_hin()
+        {
+            window.open("http://localhost/project/lang/hin.html");
+            window.close("new_search.html");
+        }
+        function open_window_tam()
+        {
+            window.open("http://localhost/project/lang/tam.html");
+            window.close("new_search.html");
+        }
+        function open_window_bts()
+        {
+            window.open("http://localhost/project/lang/bts.html");
+            window.close("new_search.html");
+        }
---
+++ project/redirect.php
@@ -0,0 +1,333 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>REDIRECT</title>
+</head>
+<body>
+    
+</body>
+
+<?php
+$n=$_POST['search_value'];
+switch($n)
+{
+    case "katchi sera":
+        {
+            $link = "<script>window.open('http://localhost/project/songs/katchi_sera.html')</script>";
+            echo $link;
+            $link_close = "<script>window.close('redirect.html')</script>";
+            echo $link_close;
+            break;
+        }
+        case "hukum":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/hukum.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "life of pazham":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/life_of_pazham.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "unakku thaan":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/unakku_thaan.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "urugi urugi":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/urugi_urugi.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "thangamey":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/thangamey.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "bae":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/bae.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "udhungada sangu":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/udhungada_sangu.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }
+        case "night changes":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/night_changes.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }   
+        case "perfect":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/perfect.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            } 
+        case "summertime sadness":
+            {
+                $link = "<script>window.open('http://localhost/project/songs/summertime_sadness.html')</script>";
+                echo $link;
+                $link_close = "<script>window.close('redirect.html')</script>";
+                echo $link_close;
+                break;
+            }   
+            case "love yourself":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/love_yourself.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }    
+            case "under my influence":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/under_my_influence.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }  
+            case "daylight":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/daylight.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }   
+            case "love me like you do":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/love_me_like_you_do.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                } 
+            case "cheap thrills":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/cheap_thrills.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }  
+            case "spring day":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/spring_day.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }  
+            case "magic shop":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/magic_shop.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                } 
+            case "butter":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/butter.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                }  
+            case "dynamite":
+                {
+                    $link = "<script>window.open('http://localhost/project/songs/dynamite.html')</script>";
+                    echo $link;
+                    $link_close = "<script>window.close('redirect.html')</script>";
+                    echo $link_close;
+                    break;
+                } 
+                case "permission to dance":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/permission_to_dance.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }  
+                case "boy with luv":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/boy_with_luv.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    } 
+                case "fire":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/fire.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }   
+                case "husn":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/husn.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    } 
+                case "zara zara":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/zara_zara.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }  
+                case "heeriye":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/heeriye.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "tum hi ho":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/tum_hi_ho.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }  
+                case "kesariya":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/kesariya.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "gundellona":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/gundellona.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }  
+                case "undiporadhey":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/undiporadhey.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "appudo ipudo":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/appudo_ipudo.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "gaali vaaluga":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/gaali_vaaluga.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "ninnila":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/ninnila.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "pilla ra":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/pilla_ra.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                case "uppenantha":
+                    {
+                        $link = "<script>window.open('http://localhost/project/songs/uppenantha.html')</script>";
+                        echo $link;
+                        $link_close = "<script>window.close('redirect.html')</script>";
+                        echo $link_close;
+                        break;
+                    }
+                 
+            default:
+            {
+                echo"<h2>CURRENTLY DEVELOPERS WORKING ON IT........<br>";
+                echo"<button onclick= def()> GO BACK TO HOMEPAGE </button>";
+                
+            }
+    
+}
+?>
+<style>
+    button{
+        color:black;
+        margin-top:30px;
+        background-color:white;
+        border:black solid 1px;
+        width: 200px;
+        height:40px;
+        transition:all ease 0.3s;
+        border-radius:10px;
+    }
+    button:hover{
+        color:white;
+        border-radius:20px;
+        background-color:black;
+    }
+
+    </style>
+<script>
+    function def()
+    {
+        window.open('http://localhost/project/homepage.html');
+        window.close('http://localhost/project/redirect.html');
+    }
+    </script>
+
+
+    </html>
